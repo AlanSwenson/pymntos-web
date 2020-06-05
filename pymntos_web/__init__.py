f4db484f6519386226c2419e3128d388234d0f60
AlanSwenson <alan@alanswenson.dev>
2020-06-05 14:24:49 -0500

    hack in stitic files

--- pymntos_web/__init__.py
+++ pymntos_web/__init__.py
@@ -13,6 +13,7 @@ from flask import (
     abort,
     flash,
     send_file,
+    send_from_directory,
 )
 
 from pymntos_web.config import Config
@@ -22,7 +23,7 @@ def create_app():
     """Create Flask app.
 
     """
-    app = Flask(__name__, static_folder="./dist/")
+    app = Flask(__name__, static_folder="static", template_folder="./dist/")
     app.config.from_object(Config)
 
     with app.app_context():
@@ -33,6 +34,40 @@ def create_app():
     def index_client():
         dist_dir = current_app.config["DIST_DIR"]
         entry = os.path.join(dist_dir, "index.html")
+        return send_file(entry)
+
+    @app.route("/js/<path:path>")
+    def send_js(path):
+        dist_dir = current_app.config["DIST_DIR"]
+        js_dir = os.path.join(dist_dir, "js/")
+        entry = os.path.join(js_dir, path)
+        return send_file(entry)
+
+    @app.route("/css/<path:path>")
+    def send_css(path):
+        dist_dir = current_app.config["DIST_DIR"]
+        css_dir = os.path.join(dist_dir, "css/")
+        entry = os.path.join(css_dir, path)
+        return send_file(entry)
+
+    @app.route("/fonts/<path:path>")
+    def send_fonts(path):
+        dist_dir = current_app.config["DIST_DIR"]
+        fonts_dir = os.path.join(dist_dir, "fonts/")
+        entry = os.path.join(fonts_dir, path)
+        return send_file(entry)
+
+    @app.route("/img/<path:path>")
+    def send_img(path):
+        dist_dir = current_app.config["DIST_DIR"]
+        img_dir = os.path.join(dist_dir, "img/")
+        entry = os.path.join(img_dir, path)
+        return send_file(entry)
+
+    @app.route("/service-worker.js")
+    def send_sw():
+        dist_dir = current_app.config["DIST_DIR"]
+        entry = os.path.join(dist_dir, "service-worker.js")
         return send_file(entry)
 
     return app
